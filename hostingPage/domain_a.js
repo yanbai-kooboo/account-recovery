8076b99c8a340b0d2ba13c1cdc9759d7d9afce34
yanbai-Kooboo <yanbai@kooboo.cn>
2016-03-23 14:19:35 +0800

    initial push

---
+++ hostingPage/domain_a.js
@@ -0,0 +1,7 @@
+function domain_a (){
+	console.log(document.documentElement.innerHTML);
+	console.log(document.getElementById("allBodyDiv"));
+	//console.log(window.parent.document);
+	//console.log(variable_a);
+	console.log(variable_b);
+}
